a865c259cca6dad4a4e00fc4538bfaa3318000f7
Marcelino <marcelinomebe@gmail.com>
2023-12-20 11:52:04 -0500

    Se personalizo la vista de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" novalidate>
         @csrf
 
         <!-- Name -->
@@ -39,14 +39,22 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex justify-between my-4">
+            <x-link
+                :href="route('login')"
+            >
+                Inciar Sesión
+            </x-link>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+            <x-link
+                :href="route('password.request')"
+            >
+                Olvidaste tu Password
+            </x-link>
         </div>
+        <x-primary-button class="w-full justify-center">
+            {{ __('Register') }}
+        </x-primary-button>
+
     </form>
 </x-guest-layout>
