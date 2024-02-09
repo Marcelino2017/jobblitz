7695640c108d7cc5f194eea8e2511eee52d1777a
Marcelino <marcelinomebe@gmail.com>
2024-02-09 11:30:17 -0500

    Se mueve el controlador a la carpeta Candidates

---
+++ app/Http/Controllers/Candidates/CandidateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Candidates;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CandidateController extends Controller
+{
+    public function index()
+    {
+        dd('Candidatos...');
+    }
+}
